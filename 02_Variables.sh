4c7f100f4f9e882933e4ee664c0037f4770ddcac
François STABLE <francois.stable@3ds.com>
2024-08-23 21:54:50 +0200

    Create shell scripts

---
+++ 02_Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PRICE_PER_APPLE=5
+MyFirstLetters=ABC
+greeting='Hello        world!'
+
+echo ${PRICE_PER_APPLE}
+echo ${MyFirstLetters}
+echo ${greeting}
+echo ${greeting}" now with spaces: ${greeting}"
